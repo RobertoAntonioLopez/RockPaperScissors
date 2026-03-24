0fd72186a883452d503eb5909d93e12e528756ea
Roberto Lopez <djgdha@gmail.com>
2026-03-24 18:44:00 -0400

    Improve code readability

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ let humanScore = 0;
 let computerScore = 0;
 
 function win(choice){
-console.log(`You WON!, the computer selected ${choice}\n`);
-humanScore++
+   console.log(`You WON!, the computer selected ${choice}\n`);
+   humanScore++
 }
 
 function lose(choice){
-console.log(`You lost!, the computer chose ${choice}\n`);
-computerScore++
+   console.log(`You lost!, the computer chose ${choice}\n`);
+   computerScore++
 }
 
 function score(){
@@ -20,14 +20,14 @@ function score(){
 function getComputerChoice(){
    let choice = Math.floor(Math.random()*3)+1;
    if(choice === 1){
-    return "rock";
-   }
-   else if(choice === 2){
-    return "paper";
-   }
-   else{
-    return "scissors"
-   }
+      return "rock";
+      }
+      else if(choice === 2){
+      return "paper";
+      }
+      else{
+      return "scissors"
+      }
 }
 
 function getHumanChoice(){
@@ -43,46 +43,46 @@ function playRound(){
    let humanChoice = getHumanChoice()
 
      if (humanChoice === null) {
-    console.log("Game cancelled by user.");
-    return false;
+         console.log("Game cancelled by user.");
+         return false;
   }
 
-   humanChoice = humanChoice.toLowerCase();
+humanChoice = humanChoice.toLowerCase();
   
 if(computerChoice === humanChoice){
 console.log("It's a draw!, you chose the same.")
 }
 else if (humanChoice === "rock"){
-if(computerChoice === "paper"){
-   lose(computerChoice);
-}else if(computerChoice === "scissors"){
-   win(computerChoice);
-}
+   if(computerChoice === "paper"){
+      lose(computerChoice);
+   }else if(computerChoice === "scissors"){
+      win(computerChoice);
+   }
 }
 
 else if (humanChoice === "paper"){
-if(computerChoice === "rock"){
-   win(computerChoice);
-}else if(computerChoice === "scissors"){
-   lose(computerChoice);
-}
+   if(computerChoice === "rock"){
+      win(computerChoice);
+   }else if(computerChoice === "scissors"){
+      lose(computerChoice);
+   }
 }
 
 else if(humanChoice === "scissors"){
-if(computerChoice === "paper"){
-   win(computerChoice);
-}else if(computerChoice === "rock"){
-   lose(computerChoice);
-}
+   if(computerChoice === "paper"){
+      win(computerChoice);
+   }else if(computerChoice === "rock"){
+      lose(computerChoice);
+   }
 }
 }
 
 function playGame(){
-   for (let i = 1; i<=5; i++)
-   {
-         console.log(`\nROUND NUMBER:${i}`)
-         playRound();  
-   }
+      for (let i = 1; i<=5; i++)
+      {
+            console.log(`\nROUND NUMBER:${i}`)
+            playRound();  
+      }
    score(); 
    return;
    
